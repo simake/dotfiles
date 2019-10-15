a85a6e1673a3b266bb135dac23427916ebc4b619
simonakesson <simonakesson@hotmail.com>
2019-10-15 14:46:51 +0200

    Added ability to toggle what files to link

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,62 @@
 #!/usr/bin/env bash
 
 mappings=(
-    ".bashrc"               "$HOME/.bashrc"
-    ".bash_profile"         "$HOME/.bash_profile"
-    ".functions"            "$HOME/.functions"
-    ".aliases"              "$HOME/.aliases"
-    ".prompt"               "$HOME/.prompt"
-    ".gitconfig"            "$HOME/.gitconfig"
-    ".spacemacs"            "$HOME/.spacemacs"
-    ".vscode.settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+#    ON      SRC                     DST
+    "NO"   ".bashrc"               "$HOME/.bashrc"
+    "NO"   ".bash_profile"         "$HOME/.bash_profile"
+    "NO"   ".functions"            "$HOME/.functions"
+    "NO"   ".aliases"              "$HOME/.aliases"
+    "NO"   ".prompt"               "$HOME/.prompt"
+    "NO"   ".gitconfig"            "$HOME/.gitconfig"
+    "NO"   ".spacemacs"            "$HOME/.spacemacs"
+    "NO"   ".vscode.settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 )
 
+# Toggle active mappings
+while true; do
+    echo -e "\nMake selections (type OK when satisfied):"
+    for (( i=0; i<${#mappings[@]}; i+=3 )); do
+        on=${mappings[i]}
+        src=${mappings[i+1]}
+        dst=${mappings[i+2]}
+        echo -e "$((i/3))) $on  $src  -->  $dst"
+    done
+    read selections
+
+    if [[ $selections =~ ^[oO][kK]$ ]]; then
+        break
+    fi
+
+    for selection in ${selections[@]}; do
+        if ! [[ $selection =~ ^[0-9]+$ ]]; then
+            continue
+        fi
+
+        idx=$(($selection * 3))
+
+        if [ $idx -ge ${#mappings[@]} ]; then
+            continue
+        fi
+
+        if [ ${mappings[idx]} == "YE" ]; then
+            mappings[idx]="NO"
+        else
+            mappings[idx]="YE"
+        fi
+    done
+done
+
+
+
 existing_files=""
-for (( i=1; i<${#mappings[@]}; i+=2 )); do
-    dst=${mappings[i]}
+for (( i=0; i<${#mappings[@]}; i+=3 )); do
+    on=${mappings[i]}
+    dst=${mappings[i+2]}
+
+    if [ "$on" == "NO" ]; then
+        continue
+    fi
+    
     if [ -e "$dst" ]; then
         existing_files="$existing_files $dst"
     fi
@@ -26,18 +69,23 @@ if [ -n "$existing_files" ]; then
     read -p "Is that okay? [y/N] " response
 
     if ! [[ "$response" =~ ^[yY][eE]?[sS]?$ ]]; then
-        echo -e "\nInstallation cancelled."
+        echo -e "\nInstallation aborted."
         exit 1
     fi
 fi
 
-echo -e "\nLinking files to home directory:"
+echo -e "\nLinking selected files."
 
 dotfiles_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo $dotfiles_dir
-for (( i=0; i<${#mappings[@]}; i+=2 )); do
-    src=${mappings[i]}
-    dst=${mappings[i+1]}
+for (( i=0; i<${#mappings[@]}; i+=3 )); do
+    on=${mappings[i]}
+    src=${mappings[i+1]}
+    dst=${mappings[i+2]}
+
+    if [ "$on" == "NO" ]; then
+        continue
+    fi
+
     mkdir -p "$(dirname "$dst")"
     ln -sf "$dotfiles_dir/$src" "$dst"
     echo "... linked $src"

c0b5f9cca63d1185abc03c8457621fb1a85f6744
simake <simonakesson@hotmail.com>
2020-07-31 00:32:26 +0200

    Add ripgrep setup scripts

---
+++ setup/ubuntu/ripgrep.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
+rm ripgrep_11.0.2_amd64.deb

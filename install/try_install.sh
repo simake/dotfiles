500ca6c99e40df38f5f1b37458c05c334b482238
simake <simonakesson@hotmail.com>
2022-01-09 22:36:56 +0100

    Fail on missing dependencies

--- install/try_install.sh
+++ install/try_install.sh
@@ -7,16 +7,21 @@ try_install() {
     # TODO: make the messages stand out
     #GREEN='\033[0;32m'
     #GREY='\033[0;37m'
+    #RED='\033[0;31m'
     #NC='\033[0m'
 
-    if deps && ! exists; then
+    if exists; then
+        #echo "${GREY}${name} already installed${NC}"
+        echo "${name} is already installed"
+    elif ! deps; then
+        #echo "${RED}${name} lacks dependencies${NC}"
+        echo "${name} lacks dependencies"
+        return 1
+    else
         #echo "${GREEN}Installing ${name}...${NC}"
         echo "Installing ${name}..."
         install
         #echo -e "${GREEN}${name} installed${NC}"
-        echo -e "${name} installed"
-    else
-        #echo "${GREY}${name} already installed${NC}"
-        echo "${name} already installed"
+        echo -e "${name} was installed"
     fi
 }

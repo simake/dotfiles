ca7dd0004fd4edd6799ea8c93d738fccd9b89c28
simake <simonakesson@hotmail.com>
2024-03-17 04:28:50 +0100

    Add lazyvim keymaps

--- misc/nvim-lazy/lua/config/keymaps.lua
+++ misc/nvim-lazy/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
+vim.keymap.set("c", "jk", "C-c")
+vim.keymap.set("c", "kj", "C-c")
+vim.keymap.set("t", "jk", "<Esc>")
+vim.keymap.set("t", "kj", "<Esc>")
+
+-- Prevent x and c from overwriting registers/clipboard
+-- vim.keymap.set("n", "x", '"_x')
+-- vim.keymap.set("v", "x", '"_x')
+-- vim.keymap.set("c", "c", '"_c')
+-- TODO: why no work?

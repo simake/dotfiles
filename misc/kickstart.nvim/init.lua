6047b1b2231d994f99b3a120ebda58c8857e2868
simake <simonakesson@hotmail.com>
2024-04-03 09:57:59 +0200

    Sort buffers by recency (actually)

--- misc/kickstart.nvim/init.lua
+++ misc/kickstart.nvim/init.lua
@@ -384,7 +384,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', function()
-        builtin.buffers { sort_lastused = true }
+        builtin.buffers { sort_lastused = true, sort_mru = true }
       end, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
